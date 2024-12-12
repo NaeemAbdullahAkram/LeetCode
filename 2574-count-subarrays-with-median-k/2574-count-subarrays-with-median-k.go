686c576a44cf46c190bdcc819769200adc4926c1
Naeem Akram <153123217+NaeemAbdullahAkram@users.noreply.github.com>
2024-12-12 09:49:44 +0500

    Time: 12 ms (12.5%), Space: 9.8 MB (75%) - LeetHub

---
+++ 2574-count-subarrays-with-median-k/2574-count-subarrays-with-median-k.go
@@ -0,0 +1,39 @@
+func countSubarrays(nums []int, k int) int {
+    n := len(nums)
+    cnt := map[int]int{}
+    p := -1
+    for i, num := range nums {
+        if num == k {
+            p = i
+            break
+        }
+    }
+    bal := 0
+    for i := p; i < n; i++ {
+        if nums[i] == k {
+            bal += 0
+        } else if nums[i] < k {
+            bal -= 1
+        } else {
+            bal += 1
+        }
+        if _, ok := cnt[bal]; !ok {
+            cnt[bal] = 1
+        } else {
+            cnt[bal]++
+        }
+    }
+    res := 0
+    bal = 0
+    for i := p; i >= 0; i-- {
+        if nums[i] == k {
+            bal += 0
+        } else if nums[i] < k {
+            bal -= 1
+        } else {
+            bal += 1
+        }
+        res += cnt[-bal] + cnt[-bal+1]
+    }
+    return res
+}
