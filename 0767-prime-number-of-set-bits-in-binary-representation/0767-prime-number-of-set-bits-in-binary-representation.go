e5f64aaddd9c0473f20be9482ff9f901b5792ef6
Naeem Akram <153123217+NaeemAbdullahAkram@users.noreply.github.com>
2024-12-31 14:38:26 +0500

    Time: 2 ms (75.61%), Space: 3.9 MB (65.85%) - LeetHub

---
+++ 0767-prime-number-of-set-bits-in-binary-representation/0767-prime-number-of-set-bits-in-binary-representation.go
@@ -0,0 +1,14 @@
+import (
+	"math/bits"
+)
+
+func countPrimeSetBits(left int, right int) (cnt int) {
+	for i := uint(left); i <= uint(right); i++ {
+		switch bits.OnesCount(i) {
+		case 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31:
+			cnt++
+		}
+	}
+
+	return
+}
